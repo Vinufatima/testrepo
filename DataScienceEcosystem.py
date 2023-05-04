62cd51046fb0ba61f5202d816dc7ab8ab8ba1835
Vinufatima <131764899+Vinufatima@users.noreply.github.com>
2023-05-05 01:06:20 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 
+# 
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+
+# Some of the commonly used libraries used by Data Scientists include:
+# <ol>
+# <li>NumPy</li>
+# <li>SciPy</li>
+# <li>Pandas</li>
+#     <li>Matplotlib</li>
+#     <li>Keras</li>
+#     <li>SciKit-Learn</li>
+#     <li>PyTorch</li>
+#     <li>Scrapy</li>
+#     <li>LightGBM</li>
+#     <li>BeautifulSoup</li>
+#     <li>ELI5</li>
+#     <li>NuPIC</li>
+#     <li>Ramp</li>
+#      <li>Pipenv</li>
+#     <li>Bob</li>
+#     <li>PyBrain</li>
+#     <li>PyTorch</li>
+#     <li>Scrapy</li>
+#     <li>LightGBM</li>
+#     <li>Caffe2</li>
+#     <li>Chainer</li>
+#    
+# </ol>
+# 
+# 
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# TensorFlow.
+# NumPy.
+# SciPy.
+# Pandas.
+# Matplotlib.
+# Keras.
+# SciKit-Learn.
+# PyTorch.
+# Scrapy.
+# BeautifulSoup.
+# LightGBM.
+# ELI5.
+# Theano.
+# NuPIC.
+# Ramp.
+# Pipenv.
+# Bob.
+# PyBrain.
+# Caffe2.
+# Chainer.
+
+# Some of the popular languages that Data Scientists use are:
+# <ol>
+# <li>Python</li>
+# <li>R</li>
+# <li>SQL</li>
+#     <li>Java</li>
+#     <li>Julia</li>
+#     <li>Scala</li>
+#     <li>C/C++</li>
+#     <li>JavaScript</li>
+# </ol>
+# 
+
+# 
+# 
+# 1.TensorFlow.
+# 2. NumPy.
+# 3. SciPy.
+# 4. Pandas.
+# 5. Matplotlib.
+# 6. Keras.
+# 7. SciKit-Learn.
+# 8. PyTorch.
+# 9. Scrapy.
+# 10. BeautifulSoup.
+# 11. LightGBM.
+# 12. ELI5.
+# 13. Theano.
+# 14. NuPIC.
+# 15. Ramp.
+# 16. Pipenv.
+# 17. Bob.
+# 18. PyBrain.
+# 19. Caffe2.
+# 20. Chainer.
+
+# |Data Science Tools| 
+# |------------------|
+# |SAS|
+# |Big ML|
+# |Apache Spark|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[6]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[7]:
+
+
+#  This will convert 200 minutes to hours by diving by 60
+days = 0
+hours = 0
+mins = 0
+
+time = 200
+#days = time / 1440
+leftover_minutes = time % 1440
+hours = leftover_minutes / 60
+#mins = time - (days*1440) - (hours*60)
+print(str(days) + " days, " + str(hours) + " hours, " + str(mins) +  " mins. ")
+
+# Result: 3.3333333333333335 hours
+
+
+# **Objectives:**
+# * R
+# * Python
+# * Java
+
+# ## Lakshmi Holla
+# 
+# ## Niveditha Pandith
+# Fatima Vinolin
+
+# In[ ]:
+
+
+
+
